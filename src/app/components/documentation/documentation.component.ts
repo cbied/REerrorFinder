79feae3a5989244228d7070f3d76e9daad687a67
Biediger <cabiediger@gmail.com>
2023-06-15 16:33:12 -0400

    update doc page

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class DocumentationComponent {
 
+  downloadFile(filename: string) {
+    // Logic to download the file
+  }
+
+  toggleAnswer(questionNumber: number) {
+    
+    console.log(questionNumber)
+    const answer: HTMLElement | null = document.getElementById(`faq-answer-${questionNumber}`);
+
+    if (answer != null) {
+      
+      console.log(answer)
+      answer.style.display = answer.style.display === 'none' ? 'block' : 'none'
+    }
+  }
+
 }
+
